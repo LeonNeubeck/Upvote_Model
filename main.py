2efb038a1e79936fa21f3cc88a6c9f3b0419de13
LeonNeubeck <Leon.neubeck240@gmail.com>
2022-11-28 11:36:14 +0900

    prediction input

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI
 from scripts import TimestampPreprocessing as tp
 import pandas as pd
 from tensorflow import keras
+from PIL import Image
+import base64
+from io import BytesIO
+import numpy as np
+from scripts.prediction import predict
 
 app = FastAPI()
+model = keras.models.load_model(f'../models/Model_predictor.h5')
+
+
+
+def base64_to_pil(img_str):
+    if "base64," in img_str:
+        img_str = img_str.split(",")[1]
+    img_raw = base64.b64decode(img_str)
+    img = Image.open(BytesIO(img_raw))
+
+    return img
 
 
 @app.get("/")
@@ -14,11 +30,21 @@ async def root():
 #Sample url = http://localhost:8000/getPrediction?timestamp=2012-04-22T04:20:11&title=the%20best%20dog%20ever&url=imgururl.jpg
 #Url output = {"prediction":0,"timestamp":"2012-04-22T04:20:11","title":"the best dog ever","url":"imgururl.jpg"}
 @app.get("/getPrediction")
-def timestampPrediction(timestamp, title, url):
-    return {'prediction':0,
-            'timestamp':timestamp,
+def timestampPrediction(time_stamp, title, image):
+
+    img = base64_to_pil(image)
+    im_size = img.size()
+    im_arr = np.array(img)
+    A,B,C = im_arr.shape
+    if C == 4:
+        im_arr = im_arr[:,:,:3]
+    result = predict(time_stamp, im_arr, im_size, title, model)
+
+    return {'prediction':result,
+            'timestamp':time_stamp,
             'title':title,
-            'url':url}
+            'image_arr':im_arr,
+            "image_size": im_size}
 
 
 @app.get("/timestampPrediction")
